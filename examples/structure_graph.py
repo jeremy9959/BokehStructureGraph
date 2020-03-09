d215b1069ae7d9e08fe78279445dcadc0f5aad0a
Jeremy Teitelbaum <jeremy.teitelbaum@uconn.edu>
2020-03-09 17:33:01 -0400

    trying a different config

--- examples/structure_graph.py
+++ examples/structure_graph.py
@@ -2,7 +2,7 @@ from bokeh.io import show
 from bokeh.plotting import figure
 from bokeh.layouts import row
 from bokeh.models import HoverTool, ColumnDataSource, TapTool, ResetTool, BoxZoomTool
-from BokehStructureGraph import BokehStructureGraph
+from BokehStructureGraph.BokehStructureGraph import BokehStructureGraph
 import numpy as np
 
 x=np.linspace(-10,10,100)
--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='BokehStructureGraph',
-    py_modules = ['BokehStructureGraph'],
-    version="0.1",
+    name='BokehStructGraph',
+    py_modules = ['BokehStructGraph'],
+    version="0.2",
     author="Jeremy Teitelbaum",
     author_email="jeremy.teitelbaum@uconn.edu",
     description="Create the structure graph of a bokeh model",
